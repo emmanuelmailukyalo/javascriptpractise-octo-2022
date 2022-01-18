050162fcfc57e7ff692a11f0b9bc56661102dfcd
EMMANUEL MAILU <emmanuel.k.mailu20@gmail.com>
2022-01-18 10:38:07 +0300

    string operators

--- operators.js
+++ operators.js
@@ -1,8 +1,22 @@
-  //   arithmetic operators
+//   arithmetic operators
         
   var num1 = 40, num2 = 10;
   document.write(num1+num2);
-  
+
   var num1 = 50;
   num1 = num1 + 5;
   document.write(num1);
+
+  var num1 = 46;
+  num1 += 5;
+  document.write(num1);
+
+  var num2 = 23
+  num2 ++;
+  document.write(num2);
+
+// string operators
+
+  var string = "Emmanuel";
+  string += "mailu";
+  console.log(string);
