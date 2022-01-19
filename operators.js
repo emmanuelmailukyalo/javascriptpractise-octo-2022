83eb6845e61eee1a051c9312c20fc99d1cce119b
EMMANUEL MAILU <emmanuel.k.mailu20@gmail.com>
2022-01-19 09:28:38 +0300

    arrays

--- operators.js
+++ operators.js
@@ -20,3 +20,19 @@
   var string = "Emmanuel";
   string += "mailu";
   console.log(string);
+
+
+
+// ARRAYS
+var arr = new Array(5);
+        arr[0] = 10;
+        arr[1] = 20;
+        arr[2] = 30;
+        arr[3] = 40;
+        arr[4] = 50;
+
+        for (i - 0; i < 85; i++) 
+        {
+        document.write(arr[i] + "<br/>");
+        }
+      
